104d4c33d08b067eca7527f83fefa2d2eaf9d792
Divinesoumyadip <soumyadipdasmahapatra343@gmail.com>
2026-02-13 09:05:22 +0530

    FIX: Correctly staging app directory

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,1 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) { return (<html><body style={{margin: 0}}>{children}</body></html>) }
+export default function RootLayout({ children }: { children: React.ReactNode }) { return (<html><body>{children}</body></html>) }
